0942f00c4830e86706c4bea463e8e05405915484
Pavel Pleshko <pavelpleshko92@mail.ru>
2022-12-19 11:16:56 +0100

    feat(yeti-demo-app): add first story for button, fix up dependencies

--- packages/yeti-demo-app/.storybook/main.js
+++ packages/yeti-demo-app/.storybook/main.js
@@ -1,1 +1,36 @@
-module.exports = { core: { builder: 'webpack5' } };
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+	core: { builder: 'webpack5' },
+	stories: ['../src/**/*.stories.{js,ts,tsx,md,mdx}'],
+	addons: [
+		'storybook-prebuilt/addon-knobs/register.js',
+		'storybook-prebuilt/addon-docs/register.js',
+		'storybook-prebuilt/addon-viewport/register.js',
+	],
+	esDevServer: {
+		// custom es-dev-server options
+		nodeResolve: true,
+		watch: true,
+		open: true,
+	},
+	webpackFinal: async config => {
+		// config.plugins.push(new RemoveAssetsPlugin({
+		//     patterns: [
+		//         // we need to remove excessive output from syntax highlighter package(more than 287 items) until its fixed.
+		//         /^react-syntax-highlighter_languages_highlight_(?!typescript|htmlbars|scss).+\.bundle\.js(\.map)?$/,
+		//     ],
+		// }));
+
+		config.resolve.plugins = [
+			...(config.resolve.plugins || []),
+			new TsconfigPathsPlugin({
+				configFile: path.resolve(__dirname, '../tsconfig.json'),
+			}),
+		];
+
+		// Return the altered config
+		return config;
+	},
+};
---
+++ packages/yeti-demo-app/src/button/button.stories.ts
@@ -0,0 +1,10 @@
+import { html } from 'lit-html';
+
+import '@yeti-wc/button';
+import { TemplateResult } from 'lit';
+
+export default {
+	title: 'yt-button',
+};
+
+export const Primary = (): TemplateResult => html` <yt-button></yt-button>`;
