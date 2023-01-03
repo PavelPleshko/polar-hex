1ffa237f6beb22a3a0847217c3e489b53eb1af8a
Pavel Pleshko <pavelpleshko92@mail.ru>
2023-01-03 14:41:23 +0100

    fix(listbox): add stories and markdown docs

--- apps/yeti-demo-app/.storybook/preview.js
+++ apps/yeti-demo-app/.storybook/preview.js
@@ -1,1 +1,17 @@
 import '!style-loader!css-loader!sass-loader!../src/styles.scss';
+
+export const parameters = {
+	actions: {
+		argTypesRegex: '^on[A-Z].*',
+	},
+	docs: {
+		inlineStories: false,
+		iframeHeight: 'auto',
+	},
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
