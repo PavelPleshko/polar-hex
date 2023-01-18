e0807e958b7faf1846e5ff88a16da3a33a443a40
Pavel Pleshko <pavelpleshko92@mail.ru>
2023-01-18 18:07:45 +0100

    fix(blog): add reading time, add post card hover effect for image

--- apps/blog/utils/filters.js
+++ apps/blog/utils/filters.js
@@ -5,4 +5,9 @@ module.exports = {
 	json: value => {
 		return JSON.stringify(value, null, 2);
 	},
+	readingTime: text => {
+		const READING_SPEED_WORDS_PER_MINUTE = 250;
+		const words = text.trim().split(/\s+/).length;
+		return Math.ceil(words / READING_SPEED_WORDS_PER_MINUTE);
+	},
 };
